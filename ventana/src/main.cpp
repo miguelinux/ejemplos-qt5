f49b847733c1d673b28ce4dae60fcf0c80dd8c47
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2019-07-20 13:08:59 -0500

    ejemplo 1

---
+++ ventana/src/main.cpp
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2019 Instituto Tecnológico Mario Molina Campus Zapopan
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+
+int main (int argc, char *argv[])
+{
+
+}
+
+/*
+ * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
+ *
+ * Local variables:
+ * c-basic-offset: 8
+ * tab-width: 8
+ * indent-tabs-mode: t
+ * End:
+ *
+ * vi: set shiftwidth=8 tabstop=8 noexpandtab:
+ * :indentSize=8:tabSize=8:noTabs=false:
+ */
