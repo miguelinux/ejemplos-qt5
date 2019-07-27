c80a1597ca2c727c53680de2282c490ccc210751
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2019-07-27 03:38:52 -0500

    add includes

--- opencv/opencv/opencvworker.h
+++ opencv/opencv/opencvworker.h
@@ -2,6 +2,10 @@
 #define OPENCVWORKER_H
 
 #include <QObject>
+#include <QImage>
+
+#include <opencv4/opencv2/core.hpp>
+#include <opencv4/opencv2/highgui.hpp>
 
 class OpenCvWorker : public QObject
 {
