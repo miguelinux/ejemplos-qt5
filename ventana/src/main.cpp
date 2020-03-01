21bda103282e60513d1708fbdacc70fded3c9ff5
Miguel Bernal Marin <miguel.bernal@zapopan.tecmm.edu.mx>
2020-03-01 00:03:41 -0600

    doing some noise

--- ventana/src/main.cpp
+++ ventana/src/main.cpp
@@ -1,24 +1,11 @@
-/*
- * Copyright (C) 2019 Instituto Tecnológico Mario Molina Campus Zapopan
- *
- * SPDX-License-Identifier: GPL-3.0-or-later
- */
+#include "mainwindow.h"
 
+#include <QApplication>
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
-
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
-
-/*
- * Editor modelines  -  https://www.wireshark.org/tools/modelines.html
- *
- * Local variables:
- * c-basic-offset: 8
- * tab-width: 8
- * indent-tabs-mode: t
- * End:
- *
- * vi: set shiftwidth=8 tabstop=8 noexpandtab:
- * :indentSize=8:tabSize=8:noTabs=false:
- */
